4749c78944cb5b6e453fc704fc7f27e080570a2b
Zahir <mailbox@a20.co.za>
2018-11-28 19:19:52 +0200

    initial commit after doing all of the published tutorials and more

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Dashboard from './views/Dashboard.vue'
+import Team from './views/Team.vue'
+import Projects from './views/Projects.vue'
 
 Vue.use(Router)
 
@@ -11,7 +13,17 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Dashboard
+    },
+    {
+      path:'/team',
+      name:'team',
+      component: Team
+    },
+    {
+      path:'/projects',
+      name:'projects',
+      component:Projects
     },
     {
       path: '/about',
